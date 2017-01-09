3329e4dfde16be30215fb2206b0dbb83d8161efb
Mr. King <Mr. Kin@MRKING>
2017-01-09 07:07:12 -0600

    Updated Migrations.

--- Vidly/Migrations/201701091159378_InitialModel.Designer.cs
+++ Vidly/Migrations/201701091159378_InitialModel.Designer.cs
@@ -7,13 +7,13 @@ namespace Vidly.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class AddIsSubscribedToCustomer : IMigrationMetadata
+    public sealed partial class InitialModel : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddIsSubscribedToCustomer));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialModel));
         
         string IMigrationMetadata.Id
         {
-            get { return "201701090604166_AddIsSubscribedToCustomer"; }
+            get { return "201701091159378_InitialModel"; }
         }
         
         string IMigrationMetadata.Source
