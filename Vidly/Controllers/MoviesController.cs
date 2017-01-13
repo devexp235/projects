790afb94f3e908793094543ab2ed9543a7a1170f
Mr. King <Mr. Kin@MRKING>
2017-01-13 07:07:52 -0600

    Built Movie controllor Form

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 using System.Data.Entity;
+using Vidly.Migrations;
 
 namespace Vidly.Controllers
 {
@@ -29,6 +30,34 @@ namespace Vidly.Controllers
             var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
+        }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
 
         public ActionResult Details(int id)
@@ -58,46 +87,68 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
-        /*public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Cusotmer 2" }
-            };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-            //return Content("Hello World
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-        }*/
-        /*public ActionResult Edit(int id)
+        [HttpPost]
+        public ActionResult Save(Movie movie)
         {
-            return Content("id=" + id);
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
+            /*public ActionResult Random()
+            {
+                var movie = new Movie() { Name = "Shrek!" };
+                var customers = new List<Customer>
+                {
+                    new Customer { Name = "Customer 1" },
+                    new Customer { Name = "Cusotmer 2" }
+                };
 
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }*/
+                var viewModel = new RandomMovieViewModel
+                {
+                    Movie = movie,
+                    Customers = customers
+                };
 
-        /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }*/
-    }
+                return View(viewModel);
+                //return Content("Hello World
+                //return HttpNotFound();
+                //return new EmptyResult();
+                //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            }*/
+            /*public ActionResult Edit(int id)
+            {
+                return Content("id=" + id);
+            }
+
+            //movies
+            public ActionResult Index(int? pageIndex, string sortBy)
+            {
+                if (!pageIndex.HasValue)
+                    pageIndex = 1;
+                if (String.IsNullOrWhiteSpace(sortBy))
+                    sortBy = "Name";
+                return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            }*/
+
+            /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+            public ActionResult ByReleaseDate(int year, int month)
+            {
+                return Content(year + "/" + month);
+            }*/
+        }
 }
